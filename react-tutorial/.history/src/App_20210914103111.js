14247841bc64b3774b1596b9252f0f0a731a1e88
chuotqb <thanhdat.dut@gmail.com>
2021-09-14 10:32:55 +0700

    update

---
+++ react-tutorial/.history/src/App_20210914103111.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Nguyễn Thành Đạt ! Học React Vui Vẻ Nhé ^^!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
